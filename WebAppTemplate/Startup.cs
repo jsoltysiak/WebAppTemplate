ff2736d4ad41ea513e3f7659377f636b6ac62087
Janusz Sołtysiak <jaso@softwaremind.pl>
2016-11-14 22:00:35 +0100

    Setup Autofac container and middleware for OWIN

--- WebAppTemplate/Startup.cs
+++ WebAppTemplate/Startup.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Reflection;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
 using Microsoft.Owin;
 using Owin;
+using WebAppTemplate;
+using WebAppTemplate.Providers;
 
-[assembly: OwinStartup(typeof(WebAppTemplate.Startup))]
+[assembly: OwinStartup(typeof(Startup))]
 
 namespace WebAppTemplate
 {
@@ -13,6 +18,27 @@ namespace WebAppTemplate
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            var builder = new ContainerBuilder();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            //builder.RegisterType<Class>().As<IClass>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new AutofacWebApiDependencyResolver(container);
+
+            var config = new HttpConfiguration
+            {
+                DependencyResolver = new AutofacWebApiDependencyResolver(container)
+            };
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(config);
+            app.UseWebApi(config);
+            app.UseAutofacMvc();
         }
     }
 }
