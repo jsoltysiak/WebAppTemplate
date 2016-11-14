2d25a4dfc9ba91ef810a07d00d5b5777de72daf0
Janusz Sołtysiak <jaso@softwaremind.pl>
2016-11-14 22:39:40 +0100

    Add unit tests project using xUnit framework

---
+++ WebAppTemplate.Tests/ValuesController_specs.cs
@@ -0,0 +1,16 @@
+﻿using WebAppTemplate.Controllers;
+using Xunit;
+
+namespace WebAppTemplate.Tests
+{
+    public class when_getting_value_by_id
+    {
+        [Fact]
+        public void then_it_should_return_value()
+        {
+            var controller = new ValuesController();
+            var result = controller.Get(1);
+            Assert.Equal("value", result);
+        }
+    }
+}
